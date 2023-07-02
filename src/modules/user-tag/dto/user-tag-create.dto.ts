17d6d38517e902ea85f3875bb17c1d67ac67d999
Anton Titov <titovanton555@gmail.com>
2023-07-02 15:33:42 +0600

    drop: remove user-tag module

--- src/modules/user-tag/dto/user-tag-create.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsNumber } from 'class-validator';
-
-export class UserTagCreateDto {
-  @ApiProperty({ example: [1, 2], type: 'number', isArray: true })
-  @IsNumber({}, { each: true })
-  @IsNotEmpty()
-  tags: number[];
-}
