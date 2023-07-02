b05829f7fd927ca6b85a6befb9b52d691d363ac0
Anton Titov <titovanton555@gmail.com>
2023-07-02 18:27:13 +0600

    feat: create Database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TagModule } from './modules/tag/tag.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
@@ -15,16 +15,7 @@ import { AuthModule } from './modules/auth/auth.module';
     UserModule,
     TagModule,
     AuthModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: Number.parseInt(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: 'test-task',
-      entities: [`${__dirname}/**/*.entity{.ts,.js}`],
-      synchronize: true,
-    }),
+    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
