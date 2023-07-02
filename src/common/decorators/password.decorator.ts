6c63307cd05ac616b6a3b579ccdcebc4a6884b71
Anton Titov <titovanton555@gmail.com>
2023-07-02 15:29:16 +0600

    chore: minor changes

--- src/common/decorators/password.decorator.ts
+++ src/common/decorators/password.decorator.ts
@@ -5,23 +5,3 @@ export const Password = () =>
     message:
       'password: Password must be 8 characters long, 1 lowercase and 1 uppercase character',
   });
-// export function Password(value: string) {
-//   if (
-//     matches(
-//       value,
-//       new RegExp('(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}'),
-//     )
-//   ) {
-//     throw new BadRequestException(
-//       'Password must be 8 characters long, 1 lowercase and 1 uppercase character',
-//     );
-//   }
-//   return Matches(
-//   ,
-//     undefined,
-//     {
-//       message:
-//         'password: Password must be 8 characters long, 1 lowercase and 1 uppercase character',
-//     },
-//   );
-// }
